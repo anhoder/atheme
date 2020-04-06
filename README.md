98374a7f61c8e9cb1772737fcc2f2e07df0007b9
AlanAlbert <alan1766447919@gmail.com>
2020-04-06 13:34:28 +0800

    :sparkles: Atheme 0.0.2. 添加看板娘、暗黑模式、Baidu分析、优化样式...

--- README.md
+++ README.md
@@ -1,17 +1,45 @@
 # atheme
 
-MWeb 新网站主题 atheme  
-[演示网站](http://alanalbert.github.io/)  
+基于MWeb编辑器的一款静态网站主题 atheme
+[演示网站1](https://danlanhai.gitee.io/)
+[演示网站1](https://alanalbert.github.io/)
+
+> 给个star吧(*╹▽╹*)
 
 ## 简介
 
-atheme是基于MWeb自带的mweb-bulma的改造版主题，具体改变有：
+atheme是基于MWeb自带的mweb-bulma的改造版主题，具体修改有：
+
+### 第一版改动
 
 * 部分排版及样式
 * 添加公告功能
 * 添加GitHub账号进行评论（utteranc）
 * 添加Google Analysis统计访问用户
 * ...
+
+### 第二版改动（最新）
+
+* 考虑谷歌需要梯子，所以添加了百度分析，需要在配置中配置百度分析ID
+* 添加暗黑模式，根据浏览器的暗黑、日间模式进行自适应
+* 取消评论模块的主题配置，修改为根据日间或暗黑模式进行自动调整
+* 添加看板娘功能，支持两种可选，猫cat及血小板plt，其中猫可以选择黑black白white两色
+* 优化移动显示
+* 优化滚动条显示
+* 部分页面调整UI并加入icon
+
+> 声明：血小板及猫并非本人原创，原作者分别为：<https://imjad.cn/>、<https://github.com/QiShaoXuan/live2DModel>
+
+
+## 预览
+
+* 暗黑模式
+
+ ![暗黑模式](./preview/dark.png)
+
+* 日间模式
+
+ ![日间模式](./preview/light.png)
 
 ## 安装
 
--- asset/app.js
+++ asset/app.js
@@ -1,21 +1,70 @@
 var i18nZHData = {
-      'tran-site-categories':'网站分类',
-      'tran-site-tags':'网站标签',
-      'tran-posted-in':'发布到',
-      'tran-tags':'标签: ',
-      'tran-archives':'归档',
-      'tran-categories':'分类: ',
-      'tran-comments':'评论',
-      'tran-readmore':'阅读全文',
-      'tran-prev-page':'&laquo; 上一页',
-      'tran-next-page':'下一页 &raquo;',
-      'tran-disqus-comments':'评论',
-      'tran-archives': '归档',
-      'tran-author': '作者：',
-      'tran-theme': '主题：',
-      'tran-notice': '公告'
-    };
+  'tran-site-categories':'网站分类',
+  'tran-site-tags':'网站标签',
+  'tran-posted-in':'发布到',
+  'tran-tags':'标签: ',
+  'tran-archives':'归档',
+  'tran-categories':'分类: ',
+  'tran-comments':'评论',
+  'tran-readmore':'阅读全文',
+  'tran-prev-page':'&laquo; 上一页',
+  'tran-next-page':'下一页 &raquo;',
+  'tran-disqus-comments':'评论',
+  'tran-archives': '归档',
+  'tran-author': '作者：',
+  'tran-theme': '主题：',
+  'tran-notice': '公告'
+};
 
+// 监听媒体查询事件，切换暗黑模式
+var darkMode = window.matchMedia('(prefers-color-scheme: dark)');
+darkMode.addListener(handleModeChange);
+function handleModeChange(darkMode) {
+  var comment = document.getElementById('github-comment');
+  var frame = document.querySelectorAll('.utterances-frame');
+  var params = [];
+  if (frame.length > 0) params = getQueryParams(frame[0].getAttribute('src'));
+  if (darkMode.matches) {
+    if (comment) comment.setAttribute('theme', 'github-dark');
+    if (frame.length > 0 && params) {
+      params['theme'] = 'github-dark';
+      frame[0].setAttribute('src', genUrl(params));
+    }
+  } else {
+    if (comment) comment.setAttribute('theme', 'github-light');
+    if (frame.length > 0 && params) {
+      params['theme'] = 'github-light';
+      frame[0].setAttribute('src', genUrl(params));
+    }
+  }
+}
+
+/**
+ * 生成URL
+ */
+function genUrl(params) {
+  var res = '';
+  for(let key in params){
+    if (!res) res = `${key}=${params[key]}`;
+    else res += `&${key}=${params[key]}`;
+  }
+  return decodeURI(res);
+}
+
+/**
+ * 获取URL参数
+ */
+function getQueryParams(url) {
+  var vars = url.split("&");
+  var data = {};
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    data[pair[0]] = pair[1];
+  }
+  return data;
+}
+
+// DOM加载完执行
 document.addEventListener('DOMContentLoaded',function(){
   const el = document.getElementById('navbarSNSRssSwitchBtn');
   el.addEventListener('click',function(){
@@ -36,4 +85,6 @@ document.addEventListener('DOMContentLoaded',function(){
     }
   }
 
+  handleModeChange(darkMode);
 });
+
